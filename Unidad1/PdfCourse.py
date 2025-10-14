e8bf0afc5a3fb8eea7db659c6589632b595835a3
ide-la-r <ism17v2@gmail.com>
2025-10-14 21:18:37 +0200

    Finished the classes Curso, SalesPerson y VideoCourse

---
+++ Unidad1/PdfCourse.py
@@ -0,0 +1,10 @@
+from Curso import Curso
+
+class PdfCourse(Curso):
+    def __init__(self, titulo, instructor, precio, clases, pages):
+        super().__init__(titulo, instructor, precio, clases)
+        self.pages = pages
+
+
+new_course = VideoCourse("Python", "p1", 25, 5, 50)
+new_course.show_datails("Python")
